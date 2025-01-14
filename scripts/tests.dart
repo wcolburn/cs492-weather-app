4e5d29f4c26f4577ee225516230376c874fc08da
Liam Colburn <colburnw@oregonstate.edu>
2025-01-14 15:50:08 -0800

    Do TODO

--- scripts/tests.dart
+++ scripts/tests.dart
@@ -13,6 +13,9 @@ void main() async {
   Map<String, dynamic> forecastJsonData = await getJsonFromUrl(forecastUrl);
   Map<String, dynamic> forecastHourlyJsonData = await getJsonFromUrl(forecastHourlyUrl);
 
+  processForecasts(forecastJsonData["properties"]["periods"]);
+  processForecasts(forecastHourlyJsonData["properties"]["periods"]);
+
   return;
 }
 
@@ -21,10 +24,13 @@ Future<Map<String, dynamic>> getJsonFromUrl(String url) async {
   return convert.jsonDecode(r.body);
 }
 
-void processForecasts(Map<String, dynamic> forecasts){
+void processForecasts(List<dynamic> forecasts){
   // TODO: pass the array of forcasts in from main
   // For loop through the forecasts and process each forecast with the
   // processForecast function below
+  for (var forecast in forecasts) {
+    processForecast(forecast);
+  }
 }
 
 void processForecast(Map<String, dynamic> forecast){
@@ -32,5 +38,8 @@ void processForecast(Map<String, dynamic> forecast){
   // The proper values that will be useful. i.e. temperature, shortForecast, longForecast
   // for now, don't return anything, just assign values for each
   // i.e. String shortForcast = "";
-
+  String shortForecast = forecast["shortForecast"];
+  String detailedForecast = forecast["detailedForecast"];
+  String windSpeed = forecast["windSpeed"];
+  int temperature = forecast["temperature"];
 }
