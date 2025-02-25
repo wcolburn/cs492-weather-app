b89e40a61fd7bbd596d38837bc3a7759f59b43e6
Liam Colburn <colburnw@oregonstate.edu>
2025-02-25 15:38:07 -0800

    Store darkmode

--- lib/providers/dark_mode_provider.dart
+++ lib/providers/dark_mode_provider.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DarkModeProvider extends ChangeNotifier { 
   bool isDarkMode = false;
+  late SharedPreferences prefs;
+
+  DarkModeProvider() {
+    initDarkMode();
+  }
+
+  void initDarkMode() async {
+    prefs = await SharedPreferences.getInstance();
+    isDarkMode = prefs.getBool('darkMode') ?? false;
+    notifyListeners();
+  }
+
+  void updateDarkModePreference() async {
+    await prefs.setBool('darkMode', isDarkMode);
+  }
 
   void setDarkMode() {
     isDarkMode = true;
+    updateDarkModePreference();
     notifyListeners();
   }
 
   void setLightMode() {
     isDarkMode = false;
+    updateDarkModePreference();
     notifyListeners();
   }
 }
